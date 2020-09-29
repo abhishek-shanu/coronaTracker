ee25194f7b4cc243fab9f4a5b6dab71349c7ed4a
Abhishek Kumar <abhishekshanu619@gmail.com>
2020-09-30 00:44:02 +0530

    Updates

---
+++ precache-manifest.172464ead21b2e68fa66ce5e875e17b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c7bd452965ef38611466ab91be9f2a",
+    "url": "/coronaTracker/index.html"
+  },
+  {
+    "revision": "b3009bea258d2d0e24ef",
+    "url": "/coronaTracker/static/css/main.a2ad809c.chunk.css"
+  },
+  {
+    "revision": "2a96de79d027232e72a6",
+    "url": "/coronaTracker/static/js/2.9253b87b.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/coronaTracker/static/js/2.9253b87b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3009bea258d2d0e24ef",
+    "url": "/coronaTracker/static/js/main.b66f9344.chunk.js"
+  },
+  {
+    "revision": "48302817472e59313646",
+    "url": "/coronaTracker/static/js/runtime-main.ca1bc29d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronaTracker/static/media/image.d7265326.png"
+  }
+]);
